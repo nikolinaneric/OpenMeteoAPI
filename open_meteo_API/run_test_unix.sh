f9dfee41534b87062d9ee43382055679b46fc3ac
nikolinaneric <nericnikolina@gmail.com>
2023-05-12 09:02:52 +0200

    added migrations to build script

--- open_meteo_API/run_test_unix.sh
+++ open_meteo_API/run_test_unix.sh
@@ -1,4 +1,6 @@
 python3 -m venv venv
-source /venv/bin/activate
+source venv/bin/activate
 pip install -r requirements.txt
+python3 manage.py makemigrations
+python3 manage.py migrate
 pytest --ds=open_meteo_API.test_settings
--- open_meteo_API/run_test_windows.bat
+++ open_meteo_API/run_test_windows.bat
@@ -1,4 +1,7 @@
 call python -m venv venv
-call %PROJECT_DIRECTORY%\venv\Scripts\activate.bat
+call venv\Scripts\activate.bat
 call pip install -r requirements.txt
+call python manage.py makemigrations
+call python manage.py migrate
 call pytest --ds=open_meteo_API.test_settings
+
